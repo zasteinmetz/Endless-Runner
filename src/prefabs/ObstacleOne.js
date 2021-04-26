5ac362c0c66bd0dc1b5502b133e7ddd6de68c7e5
Zackary <zackarydsteinmetz@gmail.com>
2021-04-26 16:01:56 -0700

    Added Obstacles

---
+++ src/prefabs/ObstacleOne.js
@@ -0,0 +1,19 @@
+class ObstacleOne extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue){
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);   // add to existing scene
+        this.moveSpeed = 4;
+    }
+    update(){
+        // move obstacle left
+        this.x -= this.moveSpeed;
+        // wrap around from left to right edge
+        if(this.x <= 0 - this.width) {
+            this.reset();
+        }
+    }
+    // position reset
+    reset(){
+        this.x = game.config.width;
+    }
+}
--- src/scenes/PlayOne.js
+++ src/scenes/PlayOne.js
@@ -4,13 +4,20 @@ class PlayOne extends Phaser.Scene {
     }
     preload(){ 
         // load images/title sprites
+        this.load.image('car', './assets/Car.png');
+        this.load.image('policeCar', './assets/PoliceCar.png');
     }
     create(){
         // place starfield
         this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0);
 
         // add player
-       this.player = new Player(this, borderUISize + borderPadding, game.config.height/2, 'player').setOrigin(0.5, 0);
+       this.player = new Player(this, borderUISize + borderPadding, game.config.height/2, 'car').setOrigin(0.5, 0);
+
+       // add obstacleOne (x3)
+       this.obstacleOne01 = new ObstacleOne(this, game.config.width + borderUISize*6, borderUISize*4, 'policeCar', 0, 30).setOrigin(0,0);
+       this.obstacleOne02 = new ObstacleOne(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'policeCar', 0, 20).setOrigin(0,0);
+       this.obstacleOne03 = new ObstacleOne(this, game.config.width, borderUISize*6 + borderPadding*4, 'policeCar', 0, 10).setOrigin(0,0);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -19,7 +26,10 @@ class PlayOne extends Phaser.Scene {
     update(){
         this.background.tilePositionX -= backgroundSpeed;
 
-        // update player
+        // update
         this.player.update();
+        this.obstacleOne01.update();
+        this.obstacleOne02.update();
+        this.obstacleOne03.update();
     }
 }
